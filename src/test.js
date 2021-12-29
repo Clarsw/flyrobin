4d6d57f0f18b7bf7a4cf0538b86ade86d0082187
clarsw <734842723@qq.com>
2021-12-29 21:09:09 +0800

    fix:fix first commit

--- src/test.js
+++ src/test.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import flyrobin from 'flyrobin'
-import flyrobin from './index'
+import flyrobin from 'flyrobin'
+// import flyrobin from './index'
 
-// import 'flyrobin/lib/theme/index.css'
-import '../lib/theme/index.css'
+import 'flyrobin/lib/theme/index.css'
+// import '../lib/theme/index.css'
 Vue.use(flyrobin)
 
 new Vue({
